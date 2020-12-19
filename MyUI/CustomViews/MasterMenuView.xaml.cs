8aba0a1539a7f642eb93481423f31a7f216955ef
Ajay Nagar <ajaynagar.9893@gmail.com>
2020-12-19 23:26:00 +0530

    Add project files.

---
+++ MyUI/CustomViews/MasterMenuView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyUI.CustomViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterMenuView : Frame
+    {
+
+        public MasterMenuView()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenClose()
+        {
+            lblApp.IsVisible = lblFacility.IsVisible
+                             = lblHome.IsVisible
+                             = lblSchool.IsVisible
+                             = lblSetting.IsVisible = (!lblApp.IsVisible);
+
+            suggetionLbl.Text = lblApp.IsVisible ? "<_Collapse Menu" : "Expand Menu_>";
+        }
+
+        private void ExpandCollpase_click(object sender, EventArgs e)
+        {
+            OpenClose();
+        }
+
+        private void Right_Swiped(object sender, SwipedEventArgs e)
+        {
+            lblApp.IsVisible = lblFacility.IsVisible
+                             = lblHome.IsVisible
+                             = lblSchool.IsVisible
+                             = lblSetting.IsVisible = true;
+            suggetionLbl.Text = "Collapse Menu";
+        }
+
+        private void Left_Swiped(object sender, SwipedEventArgs e)
+        {
+            if (this.IsVisible)
+            {
+                lblApp.IsVisible = lblFacility.IsVisible
+                                 = lblHome.IsVisible
+                                 = lblSchool.IsVisible
+                                 = lblSetting.IsVisible = false;
+                suggetionLbl.Text = "Expand Menu";
+            }
+            else
+            {
+                this.IsVisible = false;
+            }
+        }
+    }
+}
---
+++ MyUI/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Tapped(object sender, EventArgs e)
+        {
+            menu.IsVisible = !menu.IsVisible;
+        }
+    }
+}
